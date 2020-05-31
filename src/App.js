8626b1e58ce11d93e9fdb5bfbfb3e07dd428f8e7
bvvinai <64252262+bvvinai@users.noreply.github.com>
2020-05-31 09:03:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Welcome from "./Welcome.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./styles.css";
+
+function Welcome() {
+  return (
+    <div id="welcome">
+      <div className="tdiv" />
+
+      <div className="t">Hi, I'm Vinai </div>
+
+      <div className="b">Full Stack Developer...</div>
+
+      <div className="bdiv" />
+    </div>
+  );
+}
+
+export default Welcome;
